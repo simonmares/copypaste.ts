859181cfd91b421a3383a5a2d7a93ebeefe0a733
Šimon Mareš <simon.mares@protonmail.com>
2019-11-13 09:06:25 +0100

    Rename values.ts -> dateutils.ts

--- src/dateutils.ts
+++ src/dateutils.ts
@@ -1,9 +1,10 @@
 export function isDateValid(d: Date): boolean {
-  // > d2
+  // d = new Date('invalid')
+  // > d
   // Invalid Date
   // > Number.isNaN(d)
   // false
-  // > d2.getTime()
+  // > d.getTime()
   // NaN
   // >
   return Number.isNaN(d.getTime());
