09b12f0b6482ab4ef40bb7da88c69652a8a2196b
Šimon Mareš <simon.mares@protonmail.com>
2019-11-13 08:44:43 +0100

    Update values.ts (format)

--- src/values.ts
+++ src/values.ts
@@ -1,10 +1,10 @@
 export function isDateValid(d: Date): boolean {
-	// > d2
-	// Invalid Date
-	// > Number.isNaN(d)
-	// false
-	// > d2.getTime()
-	// NaN
-	// >
-	return Number.isNaN(d.getTime());
+  // > d2
+  // Invalid Date
+  // > Number.isNaN(d)
+  // false
+  // > d2.getTime()
+  // NaN
+  // >
+  return Number.isNaN(d.getTime());
 }
