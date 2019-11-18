cbdb7d379cd8b79a7bfe3f3d80a5d6f307cc1f3a
Šimon Mareš <simon.mares@protonmail.com>
2019-11-18 14:06:57 +0100

    Add valutils

---
+++ src/valutils.test.ts
@@ -0,0 +1,30 @@
+import * as unit from './valutils';
+
+test('coerceNum', () => {
+  const { coerceNum } = unit;
+  // ok
+  expect(coerceNum(111, 123)).toEqual(111);
+  // err
+  expect(coerceNum('number', 123)).toEqual(123);
+});
+
+test('coerceInt', () => {
+  const { coerceInt } = unit;
+  // ok
+  expect(coerceInt(111, 123)).toEqual(111);
+  expect(coerceInt(0.2, 123)).toEqual(0);
+  // err
+  expect(coerceInt('', 123)).toEqual(123);
+  expect(coerceInt({}, 123)).toEqual(123);
+});
+
+test('coerceStr', () => {
+  const { coerceStr } = unit;
+  // ok
+  expect(coerceStr('111', 'defaultStr')).toEqual('111');
+  // (can be coerced)
+  expect(coerceStr(111, 'defaultStr')).toEqual('111');
+  // err
+  expect(coerceStr({}, 'defaultStr')).toEqual('defaultStr');
+  expect(coerceStr({})).toEqual('');
+});
---
+++ src/valutils.ts
@@ -0,0 +1,31 @@
+export function coerceNum(val: unknown, defaultVal: number): number {
+  const asNumber = Number(val);
+  if (Number.isNaN(asNumber)) {
+    return defaultVal;
+  }
+  return asNumber * 1;
+}
+
+export function coerceInt(val: unknown, defaultVal: number): number {
+  if (val === '') {
+    return defaultVal;
+  }
+  const valAsNum = (() => {
+    const asNumber = Number(val);
+    if (Number.isNaN(asNumber)) {
+      return defaultVal;
+    }
+    return asNumber;
+  })();
+  return Number(valAsNum.toFixed(0));
+}
+
+export function coerceStr(val: unknown, defaultVal: string = ''): string {
+  if (typeof val === 'string') {
+    return val;
+  }
+  if (typeof val === 'number') {
+    return String(val);
+  }
+  return defaultVal;
+}
